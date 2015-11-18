4c3ebaff169fe8ec54222092d699a5df12146782
Sergej Klimenko <sergej.klimenko@gmail.com>
2015-11-18 12:05:01 +0200

    Add money functions

--- src/s2_money.erl
+++ src/s2_money.erl
@@ -14,6 +14,10 @@
 -export([ sub/2
         , add/2
         , is_zero/1
+        , mult/2
+        , divide/2
+        , to_money/1
+        , minus/1
         ]).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -32,8 +36,21 @@ add(A, B) ->
   D = s2_decimal:add(to_money(A), to_money(B)),
   erlang:binary_to_float(s2_decimal:to_binary(D)).
 
+mult(A, B) ->
+  D = s2_decimal:mult(to_money(A), to_money(B)),
+  erlang:binary_to_float(s2_decimal:to_binary(D)).
+
+divide(A, B) ->
+  Opts = #{precision => 2, rounding => round_down},
+  D = s2_decimal:divide(to_money(A), to_money(B), Opts),
+  erlang:binary_to_float(s2_decimal:to_binary(D)).
+
 is_zero(A) ->
   s2_decimal:is_zero(to_money(A)).
+
+minus(A) ->
+  B = s2_decimal:minus(to_money(A)),
+  erlang:binary_to_float(s2_decimal:to_binary(B)).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Internal Functions
